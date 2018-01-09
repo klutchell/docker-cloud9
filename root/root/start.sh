a019003fb6c252651f84b8b0847fcacc93cb691d
Kyle Harding <kylemharding@gmail.com>
2018-01-09 23:03:35 +0000

    abandon abc user; just go with root for now

--- root/root/start.sh
+++ root/root/start.sh
@@ -1,33 +1,19 @@
 #!/bin/bash
 
-# move home directory and put link in original location
-if [ ! -d "/data/abc" ]
-then
-	mv "/home/abc" "/data/abc"
-	ln -s "/data/abc" "/home/abc"
-fi
-
 # create ssh dir if it does not exist
-if [ ! -d "/data/abc/.ssh" ]
-then
-	mkdir -p "/data/abc/.ssh"
-fi
+[ -d "/data/.ssh" ] ||
+	mkdir -p "/data/.ssh"
 
 # generate id_rsa if it does not exist
-if [ ! -f "/data/abc/.ssh/id_rsa" ]
-then
-	ssh-keygen -q -t "rsa" -N '' -f "/data/abc/.ssh/id_rsa"
-fi
+[ -f "/data/.ssh/id_rsa" ] ||
+	ssh-keygen -q -t "rsa" -N '' -f "/data/.ssh/id_rsa"
 
 # touch authorized_keys if it does not exist
-if [ ! -f "/data/abc/.ssh/authorized_keys" ]
-then
-	touch "/data/abc/.ssh/authorized_keys"
-fi
+[ -f "/data/.ssh/authorized_keys" ] ||
+	touch "/data/.ssh/authorized_keys"
 
 # set permissions on ssh dir
-chown -R abc:abc "/data/abc/.ssh"
-chmod -R 700 "/data/abc/.ssh"
+chmod -R 700 "/data/.ssh"
 
 # start multiple processes with supervisor
-supervisord -c /config/supervisord.conf
+supervisord -c "/config/supervisord.conf"
