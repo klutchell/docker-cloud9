df5081dfb0b15b5360833eaa6dc2987c91f1fc4d
Kyle Harding <kylemharding@gmail.com>
2018-01-10 02:14:19 +0000

    move builtin root dir to /data

---
+++ root/usr/local/bin/start.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# do some things that couldn't be done until
+# the /data dir was mounted
+
+readonly home_dir="/data/home"
+readonly ssh_dir="${home_dir}/.ssh"
+
+if [ ! -d /data ]
+then
+	echo "/data must be mounted to continue!"
+	exit 1
+fi
+
+# move home dir to /data mount
+[ -d "${home_dir}" ] ||
+	cp -a "/root" "${home_dir}"
+
+# replace builtin home dir with link
+rm -rf /root
+ln -s "${home_dir}" "/root"
+	
+# create ssh dir if it does not exist
+[ -d "${ssh_dir}" ] ||
+	mkdir -p "${ssh_dir}"
+
+# generate id_rsa if it does not exist
+[ -f "${ssh_dir}/id_rsa" ] ||
+	ssh-keygen -q -t "rsa" -N '' -f "${ssh_dir}/id_rsa"
+
+# touch authorized_keys if it does not exist
+[ -f "${ssh_dir}/authorized_keys" ] ||
+	touch "${ssh_dir}/authorized_keys"
+
+# set permissions on ssh dir
+chown -R root:root "${ssh_dir}"
+chmod -R 700 "${ssh_dir}"
+
+# start multiple processes with supervisor
+supervisord -c "/config/supervisord.conf"
