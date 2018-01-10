729b68e28c4651f50803cbd15a40c017a5f0d789
Kyle Harding <kylemharding@gmail.com>
2018-01-10 17:56:09 +0000

    move overlay files again; use env vars for config paths

--- root/usr/local/bin/start.sh
+++ root/usr/local/bin/start.sh
@@ -1,36 +1,14 @@
 #!/bin/bash
 
-# do some things that couldn't be done until
-# the /data dir was mounted
-
-readonly home_dir="/data/home"
-readonly ssh_dir="${home_dir}/.ssh"
-
-if [ ! -d /data ]
+# create custom home dir if one does not exist
+if [ ! -d "${HOME_DIR}" ]
 then
-	echo "/data must be mounted to continue!"
-	exit 1
+	cp -a "/root" "${HOME_DIR}" || exit 1
 fi
-
-# move home dir to /data mount
-[ -d "${home_dir}" ] ||
-	cp -a "/root" "${home_dir}"
 
 # replace builtin home dir with link
 mv /root /root.orig
-ln -s "${home_dir}" "/root"
-	
-# create ssh dir if it does not exist
-[ -d "${ssh_dir}" ] ||
-	mkdir -p "${ssh_dir}"
-
-# touch authorized_keys if it does not exist
-[ -f "${ssh_dir}/authorized_keys" ] ||
-	touch "${ssh_dir}/authorized_keys"
-
-# set permissions on ssh dir
-chown -R root:root "${ssh_dir}"
-chmod -R 700 "${ssh_dir}"
+ln -s "${HOME_DIR}" "/root"
 
 # start multiple processes with supervisor
-supervisord -c "/etc/supervisord.conf"
+supervisord -c ${SUPERVISOR_CONFIG}
