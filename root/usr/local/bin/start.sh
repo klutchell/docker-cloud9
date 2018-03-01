df28932230aaf9268a7375fda50899c8291703b0
Kyle Harding <kylemharding@gmail.com>
2018-03-01 21:17:40 +0000

    fix order of startup steps

--- root/usr/local/bin/start.sh
+++ root/usr/local/bin/start.sh
@@ -18,21 +18,21 @@ then
 	mkdir -p "/data/docker"
 fi
 
-# generate ssh key if one does not exist
-if [ ! -f "/root/.ssh/id_rsa" ]
-then
-	ssh-keygen -q -t "rsa" -N '' -f "/root/.ssh/id_rsa" -C "$(id -un)@$(hostname) $(date)"
-fi
-
-# set permissions on ssh dir
-chown -R root:root "/data/.ssh"
-chmod -R 700 "/data/.ssh"
-
 # link to persistent ssh dir
 if [ ! -L "/root/.ssh" ]
 then
 	ln -s "/data/.ssh" "/root/.ssh"
 fi
+
+# generate ssh key if one does not exist
+if [ ! -f "/data/.ssh/id_rsa" ]
+then
+	ssh-keygen -q -t "rsa" -N '' -f "/data/.ssh/id_rsa" -C "$(id -un)@$(hostname) $(date)"
+fi
+
+# set permissions on ssh dir
+chown -R root:root "/data/.ssh"
+chmod -R 700 "/data/.ssh"
 
 # generate host keys
 /usr/bin/ssh-keygen -A
