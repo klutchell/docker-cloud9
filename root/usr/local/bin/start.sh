def2d51d71b05bdb757b3e3ad23e6c7fb62e528f
Kyle Harding <kylemharding@gmail.com>
2018-03-02 14:03:39 +0000

    leave it up to the user to create a persistent ssh dir

--- root/usr/local/bin/start.sh
+++ root/usr/local/bin/start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# create persistent ssh dir if it doesn't exist
-if [ ! -d "/data/.ssh" ]
-then
-	mkdir -p "/data/.ssh"
-fi
-
 # create c9 workspace if it doesn't exist
 if [ ! -d "/data/workspace" ]
 then
@@ -18,23 +12,7 @@ then
 	mkdir -p "/data/docker"
 fi
 
-# link to persistent ssh dir
-if [ ! -L "/root/.ssh" ]
-then
-	ln -s "/data/.ssh" "/root/.ssh"
-fi
-
-# generate ssh key if one does not exist
-if [ ! -f "/data/.ssh/id_rsa" ]
-then
-	ssh-keygen -q -t "rsa" -N '' -f "/data/.ssh/id_rsa" -C "$(id -un)@$(hostname) $(date)"
-fi
-
-# set permissions on ssh dir
-chown -R root:root "/data/.ssh"
-chmod -R 700 "/data/.ssh"
-
-# generate host keys
+# generate ssh host keys
 /usr/bin/ssh-keygen -A
 
 # manually start services
@@ -42,5 +20,3 @@ systemctl start ssh
 systemctl start cloud9.service
 systemctl start docker
 
-# remove unused containers, images, and networks
-docker system prune -f
