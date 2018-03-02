7474122c30042e8ab00bc4493173ef20c8ba6ab5
Kyle Harding <kylemharding@gmail.com>
2018-03-02 14:51:27 +0000

    install persistent ssh dir

--- root/usr/local/bin/start.sh
+++ root/usr/local/bin/start.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# create persistent ssh dir if it doesn't exist
+if [ ! -d "/data/.ssh" ]
+then
+	mkdir -p "/data/.ssh"
+fi
+
+# link to persistent ssh dir
+if [ ! -L "/root/.ssh" ]
+then
+	ln -s "/data/.ssh" "/root/.ssh"
+fi
+
+# generate ssh key if one does not exist
+if [ ! -f "/data/.ssh/id_rsa" ]
+then
+	ssh-keygen -q -t "rsa" -N '' -f "/data/.ssh/id_rsa" -C "$(id -un)@$(hostname) $(date)"
+fi
+
+# set permissions on ssh dir
+chown -R root:root "/data/.ssh"
+chmod -R 700 "/data/.ssh"
+
+# generate ssh host keys
+/usr/bin/ssh-keygen -A
+
 # create c9 workspace if it doesn't exist
 if [ ! -d "/data/workspace" ]
 then
@@ -12,11 +37,8 @@ then
 	mkdir -p "/data/docker"
 fi
 
-# generate ssh host keys
-/usr/bin/ssh-keygen -A
-
 # manually start services
 systemctl start ssh
-systemctl start cloud9.service
 systemctl start docker
+systemctl start cloud9.service
 
