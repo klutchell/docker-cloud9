5c81000f6bf4f6c8c8ec6c8a85bd61674b9efc90
Kyle Harding <kylemharding@gmail.com>
2018-05-02 16:56:01 +0000

    restore correct start script

--- start.sh
+++ start.sh
@@ -1,40 +1,7 @@
 #!/bin/sh
 
-# create ssh dir if it doesn't exist
-if [ ! -d "${HOME}/.ssh" ]
-then
-	mkdir -p "${HOME}/.ssh"
-fi
+# set timezone with TZ
+# eg. TZ=America/Toronto
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-# generate ssh key if one does not exist
-if [ ! -f "${HOME}/.ssh/id_rsa" ]
-then
-	ssh-keygen -q -t "rsa" -N '' -f "${HOME}/.ssh/id_rsa" -C "$(id -un)@$(hostname) $(date)"
-fi
-
-# generate ssh config file to prevent caching known hosts
-if [ ! -f "${HOME}/.ssh/config" ]
-then
-	cat <<EOF >> "${HOME}/.ssh/config"
-
-Host *
-	StrictHostKeyChecking no
-	UserKnownHostsFile /dev/null"
-
-EOF
-fi
-
-# set permissions on ssh dir
-chown -R root:root "${HOME}/.ssh"
-chmod -R 700 "${HOME}/.ssh"
-
-# generate ssh host keys
-/usr/bin/ssh-keygen -A
-
-# start ssh service
-if [ "$INITSYSTEM" != "on" ]
-then
-	/usr/sbin/sshd -p 22 -D
-else
-	rc-service sshd start
-fi
+node server.js -p ${C9_PORT} -w ${C9_WORKSPACE} -l 0.0.0.0 -a :
