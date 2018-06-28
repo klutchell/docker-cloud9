90a794f07d0f93ef432fbf46f67dd1e118d2651c
Kyle Harding <kylemharding@gmail.com>
2018-06-28 10:12:15 -0400

    use systemd for armhf image

--- start.sh
+++ start.sh
@@ -4,8 +4,11 @@
 # eg. TZ=America/Toronto
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-# start docker daemon in the background
-/usr/bin/dockerd --iptables=false &
+if [ "${INITSYSTEM}" != "on" ]
+then
+    # start docker daemon in the background
+    /usr/bin/dockerd --iptables=false &
+fi
 
 # start cloud9 server in the foreground
 node server.js -p ${C9_PORT} -w ${C9_WORKSPACE} -l 0.0.0.0 -a ${C9_USER}:${C9_PASS}
