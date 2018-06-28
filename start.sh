6cf4b1e5796913551e6bfbed6daed47c78179d93
Kyle Harding <kylemharding@gmail.com>
2018-06-28 12:01:12 -0400

    fix dockerd params

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 if [ "${INITSYSTEM}" != "on" ]
 then
     # start docker daemon in the background
-    /usr/bin/dockerd --iptables false --dns 8.8.8.8 &
+    /usr/bin/dockerd --iptables=false --dns 8.8.8.8 &
 fi
 
 # start cloud9 server in the foreground
